24b63b8dc75e8b588ad2db07ebb3105e8c73214e
Anmol Ali <anmolali000@icloud.com>
2024-02-28 10:37:41 -0800

    initial commit

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char characters[] = {'A', 'G', 'Q', 'Z', 'W'};
+    printf("%c\n", characters[0]);
+    printf("%c\n", characters[1]);
+    printf("%c\n", characters[2]);
+    printf("%c\n", characters[3]);
+    printf("%c\n", characters[4]);
+
+    int Numbers[] = {12, 5, 60};
+    printf("%d\n", Numbers[0]);
+    printf("%d\n", Numbers[1]);
+    printf("%d\n", Numbers[2]);
+
+    char Phrase[] = {"Sky is blue"};
+    printf("%s\n", Phrase);
+
+
+    printf("       **\n");
+    printf("      *  *\n");
+    printf("     *    *\n");
+    printf("    *      *\n");
+    printf("   *        *\n");
+    printf("  *          *\n");
+    printf(" *            *\n");
+    printf(" **************\n");
+
+
+     printf(" **************\n");
+     printf(" *            *\n");
+     printf("  *          *\n");
+     printf("   *        *\n");
+     printf("    *      *\n");
+     printf("     *    *\n");
+     printf("      *  *\n");
+     printf("       **\n");
+    return 0;
+}
