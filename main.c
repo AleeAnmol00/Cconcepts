93d4f53e8a30c8c507768207b418540c01577fe6
Anmol Ali <anmolali000@icloud.com>
2024-03-06 09:46:16 -0800

    pyramid

--- main.c
+++ main.c
@@ -6,9 +6,6 @@ int main()
    int i, j, k;
    for(i= 1; i<=9; i++){
     for(j=i; j<=9; j++) {
-        printf(" ");
-    }
-    for(k=1; k<= i; k++){
         printf("*");
     }
      printf("\n");
