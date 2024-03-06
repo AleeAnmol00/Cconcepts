e98552743254b7e2b6de02ce46c2840fdf65e53d
Anmol Ali <anmolali000@icloud.com>
2024-03-06 10:24:00 -0800

    triangle

--- main.c
+++ main.c
@@ -5,9 +5,13 @@ int main()
 {
    int i, j, k;
    for(i= 1; i<=9; i++){
-    for(j=i; j<=9; j++) {
-        printf("*");
-    }
+   for(j=i; j<=9; j++){
+    printf(" ");
+   }
+   for(k=1; k<= i; k++){
+        printf("* ");
+
+   }
      printf("\n");
    }
     return 0;
