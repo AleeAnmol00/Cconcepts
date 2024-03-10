c842c80a8de83a12a740960369ee6c7cb8fc3045
Sikander Ahmad <sikanderahmad930@gmail.com>
2024-03-10 21:46:28 +0500

    merge code

--- main.c
+++ main.c
@@ -3,16 +3,61 @@
 
 int main()
 {
-   int i, j, k;
-   for(i= 1; i<=9; i++){
-   for(j=i; j<=9; j++){
-    printf(" ");
-   }
-   for(k=1; k<= i; k++){
-        printf("*");
 
-   }
-     printf("\n");
-   }
+// <------------ code for pyramid/triangle: -------------->
+//    int i, j, k;
+//    for(i= 1; i<=9; i++){
+//    for(j=i; j<=9; j++){
+//     printf(" ");
+//    }
+//    for(k=1; k<= i; k++){
+//         printf("*");
+
+//    }
+//      printf("\n");
+//    }
+// 
+// <------------ end code for pyramid/triangle: -------------->
+
+
+// <--------- code for c concepts ------------------->
+
+// char characters[] = {'A', 'G', 'Q', 'Z', 'W'};
+//     printf("%c\n", characters[0]);
+//     printf("%c\n", characters[1]);
+//     printf("%c\n", characters[2]);
+//     printf("%c\n", characters[3]);
+//     printf("%c\n", characters[4]);
+
+//     int Numbers[] = {12, 5, 60};
+//     printf("%d\n", Numbers[0]);
+//     printf("%d\n", Numbers[1]);
+//     printf("%d\n", Numbers[2]);
+
+//     char Phrase[] = {"Sky is blue"};
+//     printf("%s\n", Phrase);
+
+
+//     printf("       **\n");
+//     printf("      *  *\n");
+//     printf("     *    *\n");
+//     printf("    *      *\n");
+//     printf("   *        *\n");
+//     printf("  *          *\n");
+//     printf(" *            *\n");
+//     printf(" **************\n");
+
+
+//      printf(" **************\n");
+//      printf(" *            *\n");
+//      printf("  *          *\n");
+//      printf("   *        *\n");
+//      printf("    *      *\n");
+//      printf("     *    *\n");
+//      printf("      *  *\n");
+//      printf("       **\n");
+
+    //  <--------------- end of c concepts -------------->
+
     return 0;
 }
