9564a24ed7ad71f17362f626e9fdff493d249171
Sikander Ahmad <sikanderahmad930@gmail.com>
2024-03-10 21:58:50 +0500

    updated code

--- main.c
+++ main.c
@@ -3,39 +3,54 @@
 
 int main()
 {
-    char characters[] = {'A', 'G', 'Q', 'Z', 'W'};
-    printf("%c\n", characters[0]);
-    printf("%c\n", characters[1]);
-    printf("%c\n", characters[2]);
-    printf("%c\n", characters[3]);
-    printf("%c\n", characters[4]);
+   int i, j, k;
+   for(i= 1; i<=9; i++){
+   for(j=i; j<=9; j++){
+    printf(" ");
+   }
+   for(k=1; k<= i; k++){
+        printf("*");
 
-    int Numbers[] = {12, 5, 60};
-    printf("%d\n", Numbers[0]);
-    printf("%d\n", Numbers[1]);
-    printf("%d\n", Numbers[2]);
+   }
+     printf("\n");
+   }
 
-    char Phrase[] = {"Sky is blue"};
-    printf("%s\n", Phrase);
+// char characters[] = {'A', 'G', 'Q', 'Z', 'W'};
+//     printf("%c\n", characters[0]);
+//     printf("%c\n", characters[1]);
+//     printf("%c\n", characters[2]);
+//     printf("%c\n", characters[3]);
+//     printf("%c\n", characters[4]);
+
+//     int Numbers[] = {12, 5, 60};
+//     printf("%d\n", Numbers[0]);
+//     printf("%d\n", Numbers[1]);
+//     printf("%d\n", Numbers[2]);
+
+//     char Phrase[] = {"Sky is blue"};
+//     printf("%s\n", Phrase);
 
 
-    printf("       **\n");
-    printf("      *  *\n");
-    printf("     *    *\n");
-    printf("    *      *\n");
-    printf("   *        *\n");
-    printf("  *          *\n");
-    printf(" *            *\n");
-    printf(" **************\n");
+//     printf("       **\n");
+//     printf("      *  *\n");
+//     printf("     *    *\n");
+//     printf("    *      *\n");
+//     printf("   *        *\n");
+//     printf("  *          *\n");
+//     printf(" *            *\n");
+//     printf(" **************\n");
 
 
-     printf(" **************\n");
-     printf(" *            *\n");
-     printf("  *          *\n");
-     printf("   *        *\n");
-     printf("    *      *\n");
-     printf("     *    *\n");
-     printf("      *  *\n");
-     printf("       **\n");
+//      printf(" **************\n");
+//      printf(" *            *\n");
+//      printf("  *          *\n");
+//      printf("   *        *\n");
+//      printf("    *      *\n");
+//      printf("     *    *\n");
+//      printf("      *  *\n");
+//      printf("       **\n");
+
+    //  <--------------- end of c concepts -------------->
+
     return 0;
 }
